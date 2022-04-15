8cb4940738494d5b3482df9d6a031e6aab521d28
AdrianoTakata <adrianostakata@gmail.com>
2022-04-14 21:59:32 -0300

    a

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.header("Acess-Control-Allow-Origin","*");
+  res.header("X-Content-Type-Options", "nosniff");
   app.use(cors());
   next();
 })
