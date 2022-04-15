0ae77f4d9b15cbe42a50c1bd6e0eaa9b26ccd444
AdrianoTakata <adrianostakata@gmail.com>
2022-04-15 01:54:42 -0300

    a

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -19,8 +19,68 @@ export default class UserRepository {
         id: userId
       },
       select: {
+        id: true,
         name: true,
-        email: true
+        email: true,
+        Profile: {
+          select: {
+            nickname: true,
+            seniority: true,
+            id: true,
+            photo: true,
+            Role: {
+              select: {
+                name: true,
+              }
+            },
+            ProfileSkill: {
+              select: {
+                skill: {
+                  select: {
+                    name: true
+                  }
+                }
+              }
+            },
+            ProfileAvailability:{
+              select:{
+                availability:{
+                  select:{
+                    id: true,
+                    day: true,
+                    start_time: true,
+                    end_time: true,
+                  }
+                }
+              }
+            }, 
+          }
+        },
+        UserSchedule:{
+          select:{
+            schedule:{
+              select:{
+                id: true,
+                day: true,
+                start_time: true,
+                end_time: true,
+                description: true
+              }
+            }
+          }
+        },
+        UserProfile:{
+          select:{
+            profile:{
+              select:{
+                id:true,
+                nickname: true,
+                seniority: true,
+                photo: true,
+              }
+            }
+          }
+        }
       }
     })
 
@@ -28,284 +88,283 @@ export default class UserRepository {
   }
 
   public async findByName(name: string) {
-    const user = await prismaClient.user.findMany({
-      where: {
-        name: {
-          startsWith: name
-        }
-      },
-      select: {
-        name: true,
-        email: true
+  const user = await prismaClient.user.findMany({
+    where: {
+      name: {
+        startsWith: name
       }
-    })
+    },
+    select: {
+      name: true,
+      email: true
+    }
+  })
 
-    return user;
-  }
+  return user;
+}
 
   public async listAllUser() {
-    const user = await prismaClient.user.findMany({
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        UserProfile:{
-          select:{
-            profile:{
-              select:{
-                nickname: true
-              }
+  const user = await prismaClient.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      UserProfile: {
+        select: {
+          profile: {
+            select: {
+              nickname: true
             }
           }
-        },
-        UserSchedule: {
-          select: {
-            schedule: {
-              select: {
-                day: true,
-                start_time: true,
-                end_time: true,
-                description: true
-              }
-            },
-          }
-        },
+        }
+      },
+      UserSchedule: {
+        select: {
+          schedule: {
+            select: {
+              day: true,
+              start_time: true,
+              end_time: true,
+              description: true
+            }
+          },
+        }
+      },
 
-        Profile: {
-          select: {
-            nickname: true,
-            seniority: true,
-            created_at: true,
-            updated_at: true,
-            description: true,
-            photo: true,
-            Address: true,
-            Role: {
-              select: {
-                name: true
-              }
-            },
-            ProfileSkill: {
-              select: {
-                skill: true
-              }
-            },
-            ProfileAvailability: {
-              select: {
-                availability: {
-                  select: {
-                    day: true,
-                    start_time: true,
-                    end_time: true
-                  }
+      Profile: {
+        select: {
+          nickname: true,
+          seniority: true,
+          created_at: true,
+          updated_at: true,
+          description: true,
+          photo: true,
+          Address: true,
+          Role: {
+            select: {
+              name: true
+            }
+          },
+          ProfileSkill: {
+            select: {
+              skill: true
+            }
+          },
+          ProfileAvailability: {
+            select: {
+              availability: {
+                select: {
+                  day: true,
+                  start_time: true,
+                  end_time: true
                 }
               }
             }
           }
-        },
-      }
-    })
+        }
+      },
+    }
+  })
 
-    return user;
-  }
+  return user;
+}
 
   public async update(name: string, email: string, password: string, userId: number) {
-    const user = await prismaClient.user.update({
-      where: {
-        id: userId
-      },
-      data: {
-        name: name,
-        email: email,
-        password: password
-      }
-    })
+  const user = await prismaClient.user.update({
+    where: {
+      id: userId
+    },
+    data: {
+      name: name,
+      email: email,
+      password: password
+    }
+  })
 
-    return user;
-  }
+  return user;
+}
 
-  public async delete(userId: number) {
-    const user = await prismaClient.user.delete({
-      where: {
-        id: userId
-      }
-    })
+  public async delete (userId: number) {
+  const user = await prismaClient.user.delete({
+    where: {
+      id: userId
+    }
+  })
 
-    return user;
-  }
+  return user;
+}
 
   public async createUserSchedule(
-    day: Date,
-    start_time: Date,
-    end_time: Date,
-    description: string,
-    userId1: number,
-    userId2: number
-  ) {
-    const UserSchedule1 = await prismaClient.userSchedule.create({
-      data: {
-        schedule: {
-          create: {
-            day: day,
-            start_time: start_time,
-            end_time: end_time,
-            description: description
-          },
+  day: Date,
+  start_time: Date,
+  end_time: Date,
+  description: string,
+  userId1: number,
+  userId2: number
+) {
+  const UserSchedule1 = await prismaClient.userSchedule.create({
+    data: {
+      schedule: {
+        create: {
+          day: day,
+          start_time: start_time,
+          end_time: end_time,
+          description: description
         },
-        user: {
-          connect: {
-            id: userId1,
-          }
+      },
+      user: {
+        connect: {
+          id: userId1,
         }
       }
-    });
+    }
+  });
 
-    const UserSchedule2 = await prismaClient.userSchedule.create({
-      data: {
-        schedule: {
-          connect: {
-            id: UserSchedule1.scheduleId
-          }
-        },
-        user: {
-          connect: {
-            id: userId2,
-          }
+  const UserSchedule2 = await prismaClient.userSchedule.create({
+    data: {
+      schedule: {
+        connect: {
+          id: UserSchedule1.scheduleId
+        }
+      },
+      user: {
+        connect: {
+          id: userId2,
         }
       }
-    })
-    return UserSchedule2;
-  }
+    }
+  })
+  return UserSchedule2;
+}
 
   public async updateUserSchedule(
-    day: Date,
-    start_time: Date,
-    end_time: Date,
-    description: string,
-    scheduleId: number,
-  ) {
-    const userSchedule = await prismaClient.schedule.update({
-      where:{
-        id: scheduleId
-      },
-      data:{
-        day: day,
-        start_time: start_time,
-        end_time: end_time,
-        description: description
-      }
-    });
-    return userSchedule;
-  }
+  day: Date,
+  start_time: Date,
+  end_time: Date,
+  description: string,
+  scheduleId: number,
+) {
+  const userSchedule = await prismaClient.schedule.update({
+    where: {
+      id: scheduleId
+    },
+    data: {
+      day: day,
+      start_time: start_time,
+      end_time: end_time,
+      description: description
+    }
+  });
+  return userSchedule;
+}
 
   public async deleteUserSchedule(scheduleId: number) {
-    const userSchedule = await prismaClient.userSchedule.findMany({
-      where:{
-        scheduleId: scheduleId
-      },
+  const userSchedule = await prismaClient.userSchedule.findMany({
+    where: {
+      scheduleId: scheduleId
+    },
+  })
+
+  for (let item of userSchedule) {
+    await prismaClient.userSchedule.delete({
+      where: {
+        id: item.id
+      }
     })
-
-    for (let item of userSchedule){
-      await prismaClient.userSchedule.delete({
-        where:{
-          id: item.id
-        }
-      })
-    }
-
-    // for (let i=0; i < userSchedule.length; i++){
-    //   await prismaClient.userSchedule.delete({
-    //     where:{
-    //       id: userSchedule[i].id
-    //     }
-    //   })
-    // }
-
-    return userSchedule;
   }
+
+  // for (let i=0; i < userSchedule.length; i++){
+  //   await prismaClient.userSchedule.delete({
+  //     where:{
+  //       id: userSchedule[i].id
+  //     }
+  //   })
+  // }
+
+  return userSchedule;
+}
 
   public async listUserByAvailability(userId: number) {
-    const user = await prismaClient.user.findUnique({
-      where:{
-        id: userId
-      },
-      select: {
-        name: true,
-        id: true,
-        email: true,
-        Profile: {
-          select: {
-            ProfileAvailability: {
-              select: {
-                availability: {
-                  select: {
-                    id: true,
-                    day: true,
-                    start_time: true,
-                    end_time: true
-                  }
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: userId
+    },
+    select: {
+      name: true,
+      id: true,
+      email: true,
+      Profile: {
+        select: {
+          ProfileAvailability: {
+            select: {
+              availability: {
+                select: {
+                  id: true,
+                  day: true,
+                  start_time: true,
+                  end_time: true
                 }
               }
             }
           }
-        },
-      }
-    })
+        }
+      },
+    }
+  })
 
-    return user;
-  }
+  return user;
+}
 
   public async listAllUserByAvailability() {
-    const users = await prismaClient.user.findMany({
-      select: {
-        name: true,
-        id: true,
-        email: true,
-        Profile: {
-          select: {
-            ProfileAvailability: {
-              select: {
-                availability: {
-                  select: {
-                    id: true,
-                    day: true,
-                    start_time: true,
-                    end_time: true
-                  }
+  const users = await prismaClient.user.findMany({
+    select: {
+      name: true,
+      id: true,
+      email: true,
+      Profile: {
+        select: {
+          ProfileAvailability: {
+            select: {
+              availability: {
+                select: {
+                  id: true,
+                  day: true,
+                  start_time: true,
+                  end_time: true
                 }
               }
             }
           }
-        },
-      }
-    })
+        }
+      },
+    }
+  })
 
-    return users;
-  }
+  return users;
+}
 
   public async listUserBySchedule(userId: number) {
-    const users = await prismaClient.userSchedule.findMany({
-      where:{
-        userId: userId
-      },
-      select:{
-        user:{},
-        schedule:{
-          include:{
-            UserSchedule:{
-              select:{
-                user:{
-                  select:{
-                    id: true,
-                    name: true,
-                    email: true,
-                    Profile:{
-                      select:{
-                        seniority: true,
-                        Role:{
-                          select:{
-                            name: true
-                          }
+  const users = await prismaClient.userSchedule.findMany({
+    where: {
+      userId: userId
+    },
+    select: {
+      user: {},
+      schedule: {
+        include: {
+          UserSchedule: {
+            select: {
+              user: {
+                select: {
+                  id: true,
+                  name: true,
+                  email: true,
+                  Profile: {
+                    select: {
+                      seniority: true,
+                      Role: {
+                        select: {
+                          name: true
                         }
                       }
                     }
@@ -314,72 +373,73 @@ export default class UserRepository {
               }
             }
           }
-        },
-      }
-    })
-    return users;
-  }
+        }
+      },
+    }
+  })
+  return users;
+}
 
   public async listAllUserBySchedule() {
-    const users = await prismaClient.user.findMany({
-      select: {
-        name: true,
-        id: true,
-        email: true,
-        UserSchedule:{
-          select:{
-            schedule:{
-              select: {
-                id: true,
-                day: true,
-                created_at: true,
-                start_time: true,
-                end_time: true, 
-                description: true
-              }
-            },
-            user:{
-              select:{
-                id: true,
-                name: true,
-                email: true
-              }
+  const users = await prismaClient.user.findMany({
+    select: {
+      name: true,
+      id: true,
+      email: true,
+      UserSchedule: {
+        select: {
+          schedule: {
+            select: {
+              id: true,
+              day: true,
+              created_at: true,
+              start_time: true,
+              end_time: true,
+              description: true
+            }
+          },
+          user: {
+            select: {
+              id: true,
+              name: true,
+              email: true
             }
           }
         }
       }
-    })
+    }
+  })
 
-    return users;
-  }
+  return users;
+}
 
   public async addContact(userId: number, profileId: number) {
-    const user = await prismaClient.userProfile.create({
-      data:{
-        user:{
-          connect: {
-            id: userId
-          }
-        },
-        profile:{
-          connect:{
-            id: profileId
-          }
+  const user = await prismaClient.userProfile.create({
+    data: {
+      user: {
+        connect: {
+          id: userId
+        }
+      },
+      profile: {
+        connect: {
+          id: profileId
         }
       }
-    })
+    }
+  })
 
-    return user;
-  }
+  return user;
+}
 
   public async removeContact(userProfileId: number) {
-    const contact = await prismaClient.userProfile.delete({
-      where:{
-        id: userProfileId
-      }
-    });
+  const contact = await prismaClient.userProfile.delete({
+    where: {
+      id: userProfileId
+    }
+  });
 
-    return contact;
-  }
+  return contact;
+}
 
 }
